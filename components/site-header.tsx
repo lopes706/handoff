2cc72e0610ec1ccadb2bf93e5dc9a4e9264be4b1
lopes706 <lopes.debs95@gmail.com>
2026-07-23 23:32:05 -0300

    Remove duplicate landing skip link

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,9 +4,6 @@ import { Brand } from "./brand";
 export function SiteHeader() {
   return (
     <header className="site-header">
-      <a className="skip-link" href="#main-content">
-        Skip to main content
-      </a>
       <Brand current />
       <nav aria-label="Primary navigation">
         <Link className="site-nav-link" href="/#how">
