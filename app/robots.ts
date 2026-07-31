ca49f84f5bedd10b5edbf706849e98b8de7cb8e5
lopes706 <lopes.debs95@gmail.com>
2026-07-30 21:53:03 -0300

    Declare robots host metadata

--- app/robots.ts
+++ app/robots.ts
@@ -3,6 +3,7 @@ import { publicEnv } from "@/lib/env";
 
 export default function robots(): MetadataRoute.Robots {
   return {
+    host: publicEnv.appUrl,
     rules: { userAgent: "*", allow: ["/", "/app"], disallow: ["/d/"] },
     sitemap: `${publicEnv.appUrl}/sitemap.xml`
   };
